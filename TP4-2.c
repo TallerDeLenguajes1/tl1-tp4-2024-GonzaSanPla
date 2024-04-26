9974f11c8a0ed999661f4d4cc359af5f00fe17df
Gonzalo Santillan Plasencia <santillangon3@gmail.com>
2024-04-26 20:55:29 -0300

    Realizado punto 3

--- TP4-2.c
+++ TP4-2.c
@@ -21,7 +21,8 @@ struct Nodo{
 Nodo* crearLista();
 Nodo* crearNodo(Nodo* siguiente);
 void agregarPendiente( Nodo** pendiente, Nodo* agregar);
-void mostrarLista(Nodo** lista);
+void mostrarLista(Nodo* lista);
+void mostraNodo(Nodo elemento);
 
 
 int main()
@@ -31,17 +32,40 @@ int main()
     Nodo* nuevo;
 
 
-    int agregar=1;
+    int opcion=20,agregar=1;
+    while (opcion!=0)
+    {
+        printf("\nElija que accion desea realizar:\n 1-Agregar una tarea a lista de tareas pendiente\n 2-Mover una/s tarea/s de la lista de pendiente a realizada\n 3-Mostrar lista de taras pendienes y lisa de tarea realizadas\n 0-Salir");
+        scanf("%d",&opcion);
 
-    while (agregar==1)
-    {   
-        nuevo=crearNodo(pendiente);
+        switch (opcion)
+        {
+        case 1:
+            while(agregar==1)
+            {   
+                nuevo=crearNodo(pendiente);
 
-        agregarPendiente(&pendiente,nuevo);
-        printf("\nDesea agragar otro elemento?\n 1-Si\n 0-No");
-        scanf("%i",&agregar);
+                agregarPendiente(&pendiente,nuevo);
+                printf("\nDesea agragar otro elemento?\n 1-Si\n 0-No");
+                scanf("%i",&agregar);
+            }
+            break;
+        case 2: //Ver como hacer lo de mover
+
+            break;
+        
+        case 3: // Verificar si funciona 
+            printf("\n-----Lista de tareas pendientes-----\n");
+            mostrarLista(pendiente);
+            
+            printf("\n-----Lista de tareas listas-----\n");
+            mostrarLista(listo);
+
+        }
+        
+        
     }
-
+    
 
 
     return(0);
@@ -69,7 +93,7 @@ Nodo* crearNodo(Nodo* siguiente)
     {
         nuevo->T.TareaID=(siguiente->T.TareaID)+1;
     }
-
+    
     printf("\nIngrese una descripcion de la nueva tarea:");
     fflush(stdin);
     gets(buffer);
@@ -93,3 +117,32 @@ void agregarPendiente( Nodo** pendiente, Nodo* agregar)
     agregar->Siguiente=*pendiente;
     *pendiente=agregar;
 }
+
+void mostrarLista(Nodo* lista)
+{
+    Nodo * aux= lista;
+    while (aux!=NULL)
+    {
+        mostraNodo(*aux);
+        aux=aux->Siguiente;
+    }
+    
+}
+
+void mostraNodo(Nodo elemento)
+{
+    printf("\nDescripcion de la tarea: %s",elemento.T.Descripcion);
+    printf("\nDuracion de la tarea: %i",elemento.T.Duracion);
+}
+
+/*
+1) Desarrolle una interfaz de carga de tareas para solicitar tareas pendientes, en la cual se
+solicite descripción y duración de la misma (el id debe ser generado automáticamente por
+el sistema, de manera autoincremental comenzando desde el número 1000). Al cabo de
+cada tarea consulte al usuario si desea ingresar una nueva tarea o finalizar la carga.
+2) Implemente una interfaz para elegir qué tareas de la lista de pendientes deben ser
+transferidas a la lista de tareas realizadas.
+3) Implemente una funcionalidad que permita listar todas las tareas pendientes y realizadas.
+4) Implemente una funcionalidad que permita consultar tareas por id o palabra clave y
+mostrarlas por pantalla, indicando si corresponde a una tarea pendiente o realizada.
+*/
