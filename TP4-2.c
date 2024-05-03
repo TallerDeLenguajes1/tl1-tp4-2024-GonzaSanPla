b82cea79f86e570e3303bffb2da0770a49f9e56d
Gonzalo Santillan Plasencia <santillangon3@gmail.com>
2024-05-03 17:21:55 -0300

    Punto 4

--- TP4-2.c
+++ TP4-2.c
@@ -11,7 +11,6 @@ int Duracion; // entre 10 – 100
 
 typedef struct Nodo Nodo;
 
-
 struct Nodo{
     Tarea T;
     Nodo* Siguiente;
@@ -23,6 +22,8 @@ Nodo* crearNodo(Nodo* siguiente);
 void agregarPendiente( Nodo** pendiente, Nodo* agregar);
 void mostrarLista(Nodo* lista);
 void mostraNodo(Nodo elemento);
+void moverDeLista(Nodo** pendiente, Nodo** realizado, int idMover);
+void buscarId(Nodo** pendiente, Nodo** realizado, int idMover);
 
 
 int main()
@@ -32,15 +33,16 @@ int main()
     Nodo* nuevo;
 
 
-    int opcion=20,agregar=1;
+    int opcion=20,agregar=1,mover=1,idMover=0,idBuscar=0,buscar=1;
     while (opcion!=0)
     {
-        printf("\nElija que accion desea realizar:\n 1-Agregar una tarea a lista de tareas pendiente\n 2-Mover una/s tarea/s de la lista de pendiente a realizada\n 3-Mostrar lista de taras pendienes y lisa de tarea realizadas\n 0-Salir");
+        printf("\nElija que accion desea realizar:\n 1-Agregar una tarea a lista de tareas pendiente\n 2-Mover una/s tarea/s de la lista de pendiente a realizada\n 3-Mostrar lista de taras pendienes y lisa de tarea realizadas\n 4-Buscar tarea por numero\n 0-Salir");
         scanf("%d",&opcion);
 
         switch (opcion)
         {
         case 1:
+            agregar=1;
             while(agregar==1)
             {   
                 nuevo=crearNodo(pendiente);
@@ -51,19 +53,53 @@ int main()
             }
             break;
         case 2: //Ver como hacer lo de mover
+            mover=1;
+            while(mover==1)
+            {
+                printf("\n-----Lista de tareas pendientes-----\n");
+                mostrarLista(pendiente);
+
+                printf("\nSeleccione el numero de la tarea que desea mover a realizada:");
+                scanf("%d",&idMover);
+
+                if (pendiente!=NULL)
+                {
+                    moverDeLista(&pendiente,&listo,idMover+1000);
+
+                    printf("\nDesea mover otro elemento?\n 1-Si\n 0-No");
+                    scanf("%i",&mover);      
+                }else
+                {
+                    printf("La lista de pendientes esta vacia :)");
+                    mover=0;
+                }
+                 
+            }
 
             break;
         
         case 3: // Verificar si funciona 
-            printf("\n-----Lista de tareas pendientes-----\n");
+            printf("\n\n-----Lista de tareas pendientes-----\n");
             mostrarLista(pendiente);
             
-            printf("\n-----Lista de tareas listas-----\n");
+            printf("\n\n-----Lista de tareas listas-----\n");
             mostrarLista(listo);
 
+            break;
+        case 4:
+            buscar=1;
+            while(buscar==1)
+            {   
+                printf("\nIngrese el numero de la tarea que desea buscar:");
+                scanf("%d",&idBuscar);
+
+                buscarId(&pendiente,&listo,idBuscar+1000);
+
+                printf("\nDesea buscar otro elemento?\n 1-Si\n 0-No");
+                scanf("%i",&buscar);
+            }
+            break;
         }
-        
-        
     }
     
 
@@ -131,8 +167,77 @@ void mostrarLista(Nodo* lista)
 
 void mostraNodo(Nodo elemento)
 {
+    printf("\nEsta es la tarea n %d",elemento.T.TareaID-1000);
     printf("\nDescripcion de la tarea: %s",elemento.T.Descripcion);
-    printf("\nDuracion de la tarea: %i",elemento.T.Duracion);
+    printf("\nDuracion de la tarea: %i\n",elemento.T.Duracion);
+}
+
+void moverDeLista(Nodo **pendiente, Nodo **realizado, int idMover)
+{
+    Nodo* aux=*pendiente;
+    Nodo* anterior=NULL;
+    int encontrado=0;
+
+    while (aux!=NULL || encontrado==0)
+    {
+        if (aux->T.TareaID==idMover)
+        {
+            if(anterior==NULL)
+            {
+                *pendiente=aux->Siguiente;
+                aux->Siguiente=*realizado;
+                *realizado=aux;
+
+                encontrado=1;
+            }else
+            {
+                anterior->Siguiente=aux->Siguiente;
+                aux->Siguiente=*realizado;
+                *realizado=aux;
+
+                encontrado=1;
+            }
+        }
+        
+        anterior=aux;
+        aux=aux->Siguiente;
+    }
+    
+}
+
+void buscarId(Nodo **pendiente, Nodo **realizado, int idBuscar)
+{
+    Nodo* aux=*pendiente;
+    int encontrado=0;
+
+    while (aux!=NULL && encontrado==0)
+    {
+        if (aux->T.TareaID==idBuscar)
+        {
+            printf("\nLa tarea fue encontrada en pendiente y es la siguiente:");
+            mostraNodo(*aux);
+            encontrado=1;
+        }
+        aux=aux->Siguiente;
+    }
+    if (encontrado==0)
+    {
+        aux=*realizado;
+        while (aux!=NULL && encontrado==0)
+        {
+            if (aux->T.TareaID==idBuscar)
+            {
+                printf("\nLa tarea fue encontrada en listo y es la siguiente:");
+                mostraNodo(*aux);
+                encontrado=1;
+            }
+            aux=aux->Siguiente;
+        }
+    }
+    if (encontrado==0)
+    {
+        printf("\nNo se encontro la tarea");
+    }
 }
 
 /*
